a45438aa174fd43ee95b6e33e71ddba7ba7d6859
rinaldo stevenazzi <rinaldostevenazzi@macbookpro.home>
2025-06-04 10:52:20 +0200

    implementation and generation

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      ["inline-import", { extensions: [".sql"] }],
+    ],
   };
 };
+// This configuration uses the `inline-import` plugin to allow importing SQL files directly into your React Native project.
