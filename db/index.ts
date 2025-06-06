28d5c75b84819f81fd9b590bfc9b493c14910ece
rinaldo stevenazzi <rinaldostevenazzi@macbookpro.home>
2025-06-06 11:03:06 +0200

    WIP - improve the db onInit function

--- db/index.ts
+++ db/index.ts
@@ -10,14 +10,4 @@ const expo = SQLite.openDatabaseSync(DB_NAME, {
   },
 });
 
-// export const db = drizzle(expo);
-
-// Uncomment the following lines if you want to use the connection object directly
 export const db = drizzle(expo, { schema });
-
-// export const db = drizzle({
-//   connection: {
-//     url: process.env.EXPO_PUBLIC_TURSO_DB_URL!,
-//     authToken: process.env.EXPO_PUBLIC_TURSO_DB_AUTH_TOKEN!,
-//   },
-// });
