37f629e6edc9d7984a21186420a8f88421252b7d
rinaldo stevenazzi <rinaldostevenazzi@macbookpro.home>
2025-06-18 16:00:53 +0200

    WIP - improve usePuzzle model view

--- modules/puzzle/play-section.tsx
+++ modules/puzzle/play-section.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import { buildChallenges } from "@/actions/puzzle-actions";
-import { Text } from "react-native";
 import PuzzleClient from "./puzzle-client";
-import { use, useEffect, useMemo, useRef } from "react";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { GameChallengeType, useGameStore } from "@/stores/game";
 
 export function PlaySection() {
   const {
@@ -20,18 +17,6 @@ export function PlaySection() {
   if (error && !isFetching) {
     throw error;
   }
-
-  const hydrated = useRef(false);
-
-  useEffect(() => {
-    if (!hydrated.current && challenges?.length) {
-      useGameStore.setState((state) => ({
-        ...state,
-        challenges: challenges as GameChallengeType[],
-      }));
-      hydrated.current = true;
-    }
-  }, [challenges]);
 
   return <PuzzleClient challenges={challenges} />;
 }
