79ff534fe51d76c8bd07974d846d1cbdba56dcaa
rinaldo stevenazzi <rinaldostevenazzi@macbookpro.home>
2025-06-23 14:11:35 +0200

    WIP - complete rectangles animations flow between screens

--- hooks/use-puzzle.ts
+++ hooks/use-puzzle.ts
@@ -1,6 +1,6 @@
 // usePuzzleViewModel.ts
 
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import {
   useGameStore,
   useGameStoreActions,
@@ -48,14 +48,14 @@ export function usePuzzle(challenges) {
     }
   }, [startTimer, timerAction]);
 
-  useEffect(() => {
-    console.log("usePuzzle - completed:", completed);
-    if (completed) {
-      timerActions.reset();
-      resetImages();
-      router.replace("/?finished=true");
-    }
-  }, [completed, resetImages, timerActions]);
+  // useEffect(() => {
+  //   console.log("usePuzzle - completed:", completed);
+  //   if (completed) {
+  //     timerActions.reset();
+  //     resetImages();
+  //     router.replace("/?finished=true");
+  //   }
+  // }, [completed, resetImages, timerActions]);
 
   useEffect(() => {
     if (challenges && challenges[currentChallengeIndex]) {
@@ -66,9 +66,17 @@ export function usePuzzle(challenges) {
     }
   }, [challenges, currentChallengeIndex]);
 
+  const onAnimationEnd = useCallback(() => {
+    timerActions.reset();
+    resetImages();
+    router.replace("/?finished=true");
+  }, [resetImages, timerActions]);
+
   return {
     image,
     pieces,
     isVertical,
+    onAnimationEnd,
+    completed,
   };
 }
